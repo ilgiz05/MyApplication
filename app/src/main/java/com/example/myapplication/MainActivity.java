e8bb95a71e928f8f2e7373e283d4bac92d3052cc
ilgiz05 <zaydamustafaeva@gmail.com>
2021-12-10 14:58:16 +0600

    Добавил текст ID

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tvTitle = findViewById(R.id.tv_title);
+        tvTitle.setText("GeekTech");
+
     }
 }
